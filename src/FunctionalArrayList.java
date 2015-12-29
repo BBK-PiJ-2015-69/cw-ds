9327490cbfb2a901b1476d2ca26ef0fc4a0a5cd8
Luke Jones <lukejones@Lukes-MBP.home>
2015-12-29 00:42:29 +0000

    added contructor and methods to FunctionalArrayList.java

---
+++ src/FunctionalArrayList.java
@@ -0,0 +1,37 @@
+/**
+* Functional Array implementation of the List interface
+*
+* @author Luke Jones
+*/
+
+public class FunctionalArrayList extends ArrayList implements FunctionalList {
+
+	private final int HEAD_INDEX = 0;
+
+	public FunctionalArrayList(int defaultSize){
+		super(defaultSize);
+	}
+
+	public FunctionalArrayList(Object[] sourceArray){
+		super(sourceArray);
+	}
+
+	public ReturnObjectImpl head(){
+		ReturnObjectImpl obj = new ReturnObjectImpl();
+		if(super.isEmpty()){
+			obj.setErrorM(ErrorMessage.EMPTY_STRUCTURE);
+			return obj;
+		}
+		obj.setObject(super.getValue(0));
+		obj.setErrorM(ErrorMessage.NO_ERROR);
+		return obj;
+	}
+
+	public FunctionalArrayList rest(){
+		FunctionalArrayList rest = new FunctionalArrayList();
+
+
+		return rest;
+	}
+
+}
