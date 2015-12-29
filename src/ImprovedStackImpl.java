4c4a38e871c949bb47d55bb6c2643c509ea98cc6
Luke Jones <lukejones@Lukes-MBP.home>
2015-12-29 00:47:43 +0000

    added constructor to ImprovedStackImpl.java

---
+++ src/ImprovedStackImpl.java
@@ -0,0 +1,15 @@
+/**
+* implementation of the Improved Stack
+*
+* @author Luke Jones
+*/
+
+public class ImprovedStackImpl implements ImprovedStack {
+
+	private List list;
+
+	public ImprovedStackImpl(){
+		this.list = list;
+	}
+
+}
