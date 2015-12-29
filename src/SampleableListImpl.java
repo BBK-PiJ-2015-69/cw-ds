eb8e8c16ee1bcd2db5460fdb68cf03df945ec2c4
Luke Jones <lukejones@Lukes-MBP.home>
2015-12-29 01:46:35 +0000

    added constructor and methods to SampleableListImpl.java

--- src/SampleableListImpl.java
+++ src/SampleableListImpl.java
@@ -1,5 +1,32 @@
 /**
-* A Sampleable implementation of the interface List
+* A Sampleable extention of Array List to implement SampleableList
 *
 * @author Luke Jones
 */
+
+public class SampleableListImpl extends ArrayList implements SampleableList {
+
+	public SampleableListImpl() {
+		super();
+	}
+
+	public SampleableList sample() {
+
+		SampleableListImpl newSampleList = new SampleableListImpl();
+		int currentSize = this.size();
+
+		if(currentSize > 0) {
+
+			Object[placeholder] = new Object[(currentSize / 2) + (currentSize % 2)];
+			int j = 0;
+			for (int i = 0; i < placeholder.length; i++) {
+				placeholder[i] = this.get(j).getReturnValue();
+				j = j +2;
+			}
+			newSampleList.setList(placeholder);
+		}
+		return newSampleList;
+
+	}
+
+}
