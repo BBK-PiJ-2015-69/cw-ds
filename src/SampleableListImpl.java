f400ba485f1b3ac6438f04f07d9eddc689d0551b
Luke Jones <lukejones@Lukes-MBP.home>
2015-12-30 08:00:16 +0000

    fleshed out methods for SampelableListImpl and added comments

--- src/SampleableListImpl.java
+++ src/SampleableListImpl.java
@@ -1,22 +1,37 @@
 /**
 * A Sampleable extention of Array List to implement SampleableList
-*
+* Allows use of either ArrayLists or LinkedLists to make a sampleable list
 * @author Luke Jones
 */
 
 public class SampleableListImpl extends ArrayList implements SampleableList {
 
+	private List list;
+
+	// CONSTRUCTOR
 	public SampleableListImpl() {
-		super();
+		this.list = null;
 	}
 
-	public SampleableList sample() {
+	public SampleableListImpl(List list) {
+		this.list = list;
+	}
 
+	// SETTER
+	public void setList(List list) {}
+		this.list = list;
+	}
+
+	// GETTER
+	public List getList() {
+		return this.list;
+	}
+
+	// RETURNS SAMPLE OF LIST (EMPTY IF EMPTY LIST)
+	public SampleableList sample() {
 		SampleableListImpl newSampleList = new SampleableListImpl();
 		int currentSize = this.size();
-
 		if(currentSize > 0) {
-
 			Object[placeholder] = new Object[(currentSize / 2) + (currentSize % 2)];
 			int j = 0;
 			for (int i = 0; i < placeholder.length; i++) {
@@ -27,6 +42,17 @@ public class SampleableListImpl extends ArrayList implements SampleableList {
 		}
 		return newSampleList;
 
+	}
+
+	// TO STRING
+	public String toString() {
+		String listString = new String();
+		if (list != null){
+			for (int i = 0; i < list.size(); i++) {
+				listString += list.get(i).getReturnVal();
+			}
+		}
+		return listString;
 	}
 
 }
